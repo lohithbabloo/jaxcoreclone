f6af7d463291fc0e8bba47357d35d590de14a505
lohithbabloo <lohithkumartangeti@gmail.com>
2023-04-27 16:51:03 +0530

    updated img to image

--- pages/components/midContainer.tsx
+++ pages/components/midContainer.tsx
@@ -59,7 +59,7 @@ export const Midcontainer = () => {
         {/* cards */}
         <div className='card-wrapper flex mt-10 items-center justify-center max-w-2xl flex-wrap gap-[30px] md:flex-row flex-col'>
           <div className='card w-[300px] h-[300px] rounded-xl flex flex-col justify-center items-center text-center gap-[10px] bg-zinc-800 text-white p-5 transistion ease-linear duration-150 transform delay-100 hover:opacity-100 hover:scale-110'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12" >
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
